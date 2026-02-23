43cd0b9104aee88aa5655e91c4787ea3aa654ab7
ashraful-hoda <asrafulhoda789@gmail.com>
2026-02-23 19:43:19 +0600

    add toogle functionality and count functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let interviewList = [];
 let rejectedList = [];
+let currentStatus = 'all-filter-btn';
 
 // 1.Button Toggling
 const allFilterBtn = document.getElementById('all-filter-btn');
@@ -39,6 +40,7 @@ function toggleStyle(id){
     else if(id == 'interview-filter-btn'){
         filterSection.classList.remove('hidden');
         allCardSection.classList.add('hidden');
+        renderInterview();
     }
     else if(id == 'rejected-filter-btn'){
         filterSection.classList.remove('hidden');
@@ -58,20 +60,37 @@ calculateCount();
 //4. Button clickable
 mainContainer.addEventListener('click',
     function(event){
-
-        const parentNode = event.target.parentNode.parentNode;
-        console.log(parentNode);
-        const companyName = document.querySelector('.titleName').innerText;
-        const regignation = document.querySelector('.regignation').innerText;
-        const description = document.querySelector('.description').innerText;
-        const statuss = document.querySelector('.statuss').innerText;
-        const summary = document.querySelector('.summary').innerText;
         
-
         if(event.target.classList.contains('interview-btn')){
+            const parentNode = event.target.parentNode.parentNode;
+            const companyName = document.querySelector('.titleName').innerText;
+            const regignation = document.querySelector('.regignation').innerText;
+            const workStyle = document.querySelector('.work-style').innerText;
+            const workShift = document.querySelector('.work-shift').innerText;
+            const salary = document.querySelector('.salary').innerText;
+            const statuss = document.querySelector('.statuss').innerText;
+            const summary = document.querySelector('.summary').innerText;
+
             parentNode.querySelector('.statuss').innerText = 'Interview';
 
+            const cardInfo = {
+                companyName,
+                regignation,
+                workStyle,
+                workShift,
+                salary,
+                statuss : 'Interview',
+                summary
+            }
+
+            const jobExist = interviewList.find(job => job.companyName == cardInfo.companyName);
+
+            if(!jobExist){
+                interviewList.push(cardInfo);
+            }
+
             
+
         }
         else if(event.target.classList.contains('rejected-btn')){
 
@@ -81,4 +100,62 @@ mainContainer.addEventListener('click',
         }
     }
 )
+
+function renderInterview(){
+    filterSection.innerHTML = '';
+
+    if(interviewList.length === 0){
+        const emptyMessage = document.createElement('div');
+        emptyMessage.className = 'text-center rounded-2xl shadow p-5 shadow-gray-300 mt-10 space-y-5';
+        emptyMessage.innerHTML = `
+            <div>
+                <i class="fa-solid fa-file"></i>
+            </div>
+            <div>
+                <h1 class="font-bold text-2xl text-color">No Jobs Available</h1>
+                <p class="font-medium text-neutral-500">Check back soon for new job opportunities</p>
+            </div>
+        `;
+
+        filterSection.appendChild(emptyMessage);
+        return;
+
+    }
+
+    for(let interview of interviewList){
+        let div = document.createElement('div');
+        div.className = 'job-card rounded-2xl shadow p-5 shadow-gray-300 flex flex-col md:flex-row justify-between';
+        div.innerHTML = `
+            <div class="space-y-5">
+                <div class="space-y-2">
+                    <h1 class="titleName text-2xl text-color font-bold"> ${interview.companyName}</h1>
+                    <h3 class="regignation text-[20px] text-neutral-500">React Native Developer</h3>
+                </div>
+
+                <ul class="description flex flex-col md:flex-row items-start md:items-center gap-3 md:gap-10 list-disc text-neutral-500">
+                    <p>${interview.workStyle}</p>
+                    <li>${interview.workShift}</li>
+                    <li>${interview.salary}</li>
+                    
+                </ul>
+
+                <div>
+                    <p class="statuss">Not Applied</p>
+                    <p class="summary">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+                </div>
+
+                <div>
+                    <button class="btn btn-accent btn-outline interview-btn text-[16px] text-bold">Interview</button>
+                    <button class="btn btn-secondary btn-outline rejected-btn text-[16px] text-bold">Rejected</button>
+                </div>
+            </div>
+
+            <div>
+                <button class="btn w-[50px] h-[50px] rounded-full delete-btn"><i class="fa-solid fa-trash"></i></button>
+            </div>
+        `;
+
+        filterSection.appendChild(div);
+    }
+}
 
